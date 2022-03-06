987668414e8962ec9da6d55d6615c930fa289554
znz <1111@qq.com>
2022-03-06 19:30:38 +0800

    cookie 实现验证码

--- login/views.py
+++ login/views.py
@@ -9,6 +9,20 @@ from login.utils import check_username, check_phone, check_password, verify_logi
 # Create your views here.
 
 
+def index(request):
+    uid = request.COOKIES.get('uid')
+    is_login = request.COOKIES.get('is_login')
+    ctx = {
+        'user': User.objects.filter(id=uid).first(),
+        'is_login': is_login
+    }
+    captcha = random.randint(1000, 9999)
+    print(captcha, 111111111111111111111111111111111111111111111111111111111)
+    response = render(request, template_name='index/index.html', context=ctx)
+    response.set_cookie(key='captcha', value=captcha)
+    return response
+
+
 def register(request):
     if request.method == 'GET':
         return render(request, template_name='register/register.html')
@@ -41,17 +55,28 @@ def register(request):
 
 def login(request):
     if request.method == 'GET':
-        return render(request, template_name='login/login.html')
+        captcha = random.randint(1000, 9999)
+        print(captcha, 111111111111111111111111111111111111111111111111111111111)
+        response = render(request, template_name='login/login.html')
+        response.set_cookie(key='captcha', value=captcha)
+        return response
     else:
         username = request.POST.get('username')
         password = request.POST.get('password')
+        code = request.POST.get('code')
+        captcha = request.COOKIES.get('captcha')
         verify_login_result = verify_login(username, password)
         if verify_login_result['code'] == 400:
             json_object = JsonResponse(verify_login_result)
             json_object.delete_cookie(key='uid')
             json_object.delete_cookie(key='is_login')
             return json_object
-        print(request.COOKIES.values(), 333333333333333333333333333333333)
+        if code != captcha:
+            content = {
+                'code': 400,
+                'prompt': '验证码不对'
+            }
+            return JsonResponse(data=content)
         response_object = render(request, template_name='blog/blog.html')
         user_object = User.objects.filter(name=username).first()
         response_object.set_cookie(key='uid', value=user_object.id, max_age=300)
@@ -75,11 +100,4 @@ def test(request, *args, **kwargs):
     return HttpResponse(222222222222222222)
 
 
-def index(request):
-    uid = request.COOKIES.get('uid')
-    is_login = request.COOKIES.get('is_login')
-    ctx = {
-        'user': User.objects.filter(id=uid).first(),
-        'is_login': is_login
-    }
-    return render(request, template_name='index/index.html', context=ctx)
+
